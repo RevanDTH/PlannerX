a719ab690c6f05949e3d4f4147e296dbb05ece94
RevanDTH <72939884+RevanDTH@users.noreply.github.com>
2025-11-15 15:41:30 +0100

    added create,delete,save function and a setup mode

--- plannerx.cpp
+++ plannerx.cpp
@@ -2,14 +2,22 @@
 
 
 #include <iostream>
+#include <fstream>
 #include <string>
 #include <Windows.h>
+#include <map>
+#include <list>
+#include <limits>
+
+
+//global variables
+std::map<int, std::string> tasklist = {};
+LPCSTR saveFilePath = "C:\\ProgramData\\PlannerX"; 
+LPCSTR saveFile = "C:\\ProgramData\\PlannerX\\savefile.plx"; 
+
 
 void setup(){
     using namespace std;
-
-    LPCSTR saveFilePath = "C:\\ProgramData\\PlannerX"; 
-    LPCSTR saveFile = "C:\\ProgramData\\PlannerX\\savefile.plx"; 
 
     cout << "Welcome to the Setup of PlannerX." << endl;
     cout << "Press [ENTER] to continue with the setup or press [ctrl] + [c] to abort this action." << endl;
@@ -36,19 +44,68 @@ void setup(){
 }
 
 void loadTasks(){
-    //to-do
+    std::ifstream fileStream(saveFile);
+    std::string line;
+    while (std::getline(fileStream,line))
+    {
+        size_t pos = line.find(';');
+        if (pos != std::string::npos) {
+            std::string idStr = line.substr(0, pos);
+            std::string taskStr = line.substr(pos + 1);
+
+            // trim helper
+            auto trim = [](std::string &s) {
+                const char* ws = " \t\n\r\f\v";
+                s.erase(0, s.find_first_not_of(ws));
+                s.erase(s.find_last_not_of(ws) + 1);
+            };
+
+            trim(idStr);
+            trim(taskStr);
+
+            try {
+                int id = std::stoi(idStr);
+                tasklist[id] = taskStr;
+            } catch (...) {
+                std::cout << "Your tasklist is corrupted, please reinstall PlannerX by using the -setup argument on excution!" << std::endl;
+            }
+        }
+    }
+    
 }
 
 void saveTasks(){
-    //to-do
+    std::ofstream fileStream(saveFile);
+    if (!fileStream.is_open()) {
+        std::cerr << "Failed to open savefile for writing: " << saveFile << std::endl;
+        return;
+    }
+
+    for (const auto& entry : tasklist)
+    {
+        fileStream << entry.first << ";" << entry.second << "\n";
+    }
+
+    fileStream.close();
 }
 
-void createTask(){
-    //to-do
+void createTask(std::string taskName){
+    using namespace std;
+
+    int highestID = 0;
+    for (const auto& entry : tasklist) {
+        if (entry.first > highestID)
+            highestID = entry.first;
+    }
+
+    int newID = highestID + 1;
+
+    tasklist[newID] = taskName;
 }
 
-void solveTask(){
-    //to-do
+
+void solveTask(int taskID){
+    tasklist.erase(taskID);
 }
 
 bool hasArgument(int argc, char const* argv[], const std::string& name)
@@ -68,13 +125,14 @@ int main(int argc, char const *argv[])
         setup();
     }
 
+    loadTasks();
+
     using namespace std;
     system("cls");
 
     int userChoice;
-    bool inMainMenu = true;
 
-    while (inMainMenu)
+    while (true)
     {
         system("cls");
 
@@ -87,28 +145,52 @@ int main(int argc, char const *argv[])
         | $$      | $$ /$$__  $$| $$  | $$| $$  | $$| $$_____/| $$      /$$/\  $$
         | $$      | $$|  $$$$$$$| $$  | $$| $$  | $$|  $$$$$$$| $$     | $$  \ $$
         |__/      |__/ \_______/|__/  |__/|__/  |__/ \_______/|__/     |__/  |__/                                                                    
-        )" << endl << endl << endl;  
+        )" << endl << endl << endl;
+        
+        cout << "------------------------------- Tasks -------------------------------" << endl;
+        for(const auto& task : tasklist){
+            cout << "(" << std::to_string(task.first) << ") " << task.second << endl << endl;
+        }
+        cout << "---------------------------------------------------------------------" << endl << endl;
 
         cout << "[1] Create task" << endl;
         cout << "[2] Solve task" << endl;
+        cout << "[3] Exit" << endl;
         cout << ": ";
-        cin >> userChoice;
+        if(cin >> userChoice){
+            string newTaskName;
+            switch (userChoice)
+            {
+            case 1:
+                system("cls");
+                cout << "Name of the Task: ";
+                getline(std::cin >> std::ws, newTaskName);
+                createTask(newTaskName);
+                break;
 
-        switch (userChoice)
+            case 2:
+                system("cls");
+                int taskID;
+                for(const auto& task : tasklist){
+                    cout << "(" << std::to_string(task.first) << ") " << task.second << endl << endl;
+                }
+                cout << "ID of the Task: ";
+                cin >> taskID;
+                solveTask(taskID);
+                break;
+            case 3:
+                saveTasks();
+                exit(0);
+            default:
+                break;
+            }
+        }else
         {
-        case 1:
-            createTask();
-            inMainMenu = false;
-            break;
-
-        case 2:
-            solveTask();
-            inMainMenu = false;
-            break;
-        
-        default:
-            break;
+            cin.clear();
+            continue;
         }
+        
+
     }
     
 
